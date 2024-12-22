c5493d3ade88bfb1f15b544104ae910f5b9201cf
Wisnu Aji Pamungkas <wisnuajipamungkas354@gmail.com>
2024-12-22 14:23:30 +0700

    Pengkodean Muda Mudi

--- app/Models/Mudamudi.php
+++ app/Models/Mudamudi.php
@@ -9,44 +9,40 @@ class Mudamudi extends Model
 {
     use HasFactory;
 
-    protected $guarded = ['id'];
+    protected $guarded = [];
 
-    // Event "creating" untuk otomatis mengisi unique_id
-    // protected static function booted()
-    // {
-    //     static::creating(function ($model) {
-    //         // Panggil fungsi untuk generate unique_id
-    //         $model->nig = self::generateUniqueId();
-    //     });
-    // }
+    // Event "creating" untuk otomatis mengisi pengkodean id
+    protected static function booted()
+    {
+        static::creating(function ($model) {
+            // Panggil fungsi untuk generate id
+            $model->id = self::generateUniqueId();
+        });
+    }
 
     // Fungsi untuk generate ID unik
-    // public static function generateUniqueId()
-    // {
-    //     // Ambil tahun dan bulan saat ini
-    //     $year = date('y'); // Dua digit tahun (contoh: 24 untuk 2024)
-    //     $month = date('m'); // Dua digit bulan (contoh: 09 untuk September)
+    public static function generateUniqueId()
+    {
+        // Ambil tahun dan bulan saat ini
+        $yearMonth = date('ym', mktime(24,0,0,date('m'),0,date('y'))); // Dua digit tahun & bulan (contoh: 2411)
 
-    //     // Ambil ID unik yang belum dipakai untuk bulan dan tahun yang sama
-    //     $lastId = self::whereYear('created_at', date('Y'))
-    //                   ->whereMonth('created_at', date('m'))
-    //                   ->max('unique_id');
+        // Ambil ID unik yang belum dipakai untuk bulan dan tahun yang sama
+        $getLastId = self::latest('id')->value('id');
+        $prefixId = substr($getLastId, 0, 4);
+        $uniqueId = 1;
 
-    //     // Jika tidak ada ID yang ditemukan, maka set ID awal menjadi 1
-    //     if (!$lastId) {
-    //         $uniqueId = 1;
-    //     } else {
-    //         // Ambil tiga digit terakhir dari unique_id yang ada dan tambahkan 1
-    //         $lastIdNumber = (int)substr($lastId, 4); // Ambil tiga digit ID unik
-    //         $uniqueId = $lastIdNumber + 1;
-    //     }
+        // Jika id terakhir sama dengan bulan dan tahun hari ini maka id terakhir + 1
+        if($prefixId == $yearMonth) { 
+            $lastIdNumber = (int)substr($getLastId, 4);
+            $uniqueId= $lastIdNumber + 1;
+        }
 
-    //     // Format ID unik menjadi tiga digit
-    //     $uniqueIdFormatted = str_pad($uniqueId, 3, '0', STR_PAD_LEFT);
+        // Format ID unik menjadi tiga digit
+        $uniqueIdFormatted = str_pad($uniqueId, 3, '0', STR_PAD_LEFT);
 
-    //     // Gabungkan tahun, bulan, dan ID unik untuk membuat ID lengkap
-    //     return $year . $month . $uniqueIdFormatted;
-    // }
+        // Gabungkan tahun, bulan, dan ID unik untuk membuat ID lengkap
+        return $yearMonth . $uniqueIdFormatted;
+    }
 
     public function daerah()
     {
