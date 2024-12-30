3c71c5ee6310ce4ec3f2d1ccd065df9d64b24139
Wisnu Aji Pamungkas <wisnuajipamungkas354@gmail.com>
2024-12-30 16:47:57 +0700

    Try PNG format QR

--- resources/views/infolists/components/qr-code-generator.blade.php
+++ resources/views/infolists/components/qr-code-generator.blade.php
@@ -1,6 +1,6 @@
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
     <div class="flex flex-col justify-center items-center">
-        {!! QrCode::size(150)->errorCorrection('H')->generate($getRecord()->id . ' | ' . $getRecord()->nama); !!}
+        <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(150)->generate('$getRecord()->id . ' | ' . $getRecord()->nama!')) !!} ">
         <span class="text-center mt-2 font-semibold">MM-ID : {{ $getRecord()->id }}</span>
     </div>
 </x-dynamic-component>
