64857aa37fc74089ca4e13fd8aeb8af5d92e1b20
Wisnu Aji Pamungkas <wisnuajipamungkas354@gmail.com>
2024-12-30 17:07:41 +0700

    Trial QR Download Link 3

--- resources/views/infolists/components/qr-code-generator.blade.php
+++ resources/views/infolists/components/qr-code-generator.blade.php
@@ -1,7 +1,7 @@
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
     <div class="flex flex-col justify-center items-center">
-        <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(150)->generate($getRecord()->id . ' | ' . $getRecord()->nama)); !!} ">
-        <a href="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(150)->generate($getRecord()->id . ' | ' . $getRecord()->nama)); !!} " download>Download QR Code</a>
+        <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(150)->margin(100)->generate($getRecord()->id . ' | ' . $getRecord()->nama)); !!} ">
+        <a href="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(150)->margin(100)->generate($getRecord()->id . ' | ' . $getRecord()->nama)); !!} " download="{{ $getRecord()->nama }}">Download QR Code</a>
         <span class="text-center mt-2 font-semibold">MM-ID : {{ $getRecord()->id }}</span>
     </div>
 </x-dynamic-component>
