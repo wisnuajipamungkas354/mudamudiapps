363a6efed59ff0790a9d4942ad36e48879028abc
Wisnu Aji Pamungkas <wisnuajipamungkas354@gmail.com>
2025-01-22 21:56:38 +0700

    Hide Kategori Peserta

--- resources/views/livewire/form-kegiatan.blade.php
+++ resources/views/livewire/form-kegiatan.blade.php
@@ -5,7 +5,7 @@
     <p><x-heroicon-o-map-pin class="inline" width="1.3rem" /> {{ $this->kegiatan->tempat_kegiatan }}</p>
     <p><x-heroicon-o-calendar class="inline" width="1.3rem"/> {{ strftime("%A, %d %B %Y", strtotime($this->kegiatan->waktu_mulai)) }}</p>
     <p><x-heroicon-o-clock class="inline" width="1.3rem"/> {{ date('H:i', strtotime($this->kegiatan->waktu_mulai)) }} s/d Selesai</p>
-    <p><x-heroicon-o-users class="inline" width="1.3rem"/> {{ $this->kegiatan->kategori_peserta }}</p>
+    <!-- <p><x-heroicon-o-users class="inline" width="1.3rem"/> {{ $this->kegiatan->kategori_peserta }}</p> -->
     
     <p class="text-center mt-3">Silahkan pilih dibawah ini :</p>
     <div class="flex flex-col gap-2 mt-5 w-full">
